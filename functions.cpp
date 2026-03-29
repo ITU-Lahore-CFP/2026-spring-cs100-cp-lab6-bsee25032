ac5fa7b2803a1f45a2d787b9367d3ad06c8f2f77
bsee25032 <bsee25032@itu.edu.pk>
2026-03-29 23:59:56 +0500

    task done

--- functions.cpp
+++ functions.cpp
@@ -3,12 +3,20 @@
 
 int sumOfSquares(int n) 
 {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i * i;
+    }
+    return sum;
 }
 
 int findFactorial(int n)
 {
+    if (n == 0) {
+        return 1;
+    }
+    return n * findFactorial(n - 1);
 }
-
 
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 
 
 int main() {
+     int n;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> n;
+
+    int sumSquares = sumOfSquares(n);
+    int factorial = findFactorial(n);
+
+    std::cout << "Sum of squares from 1 to " << n << " is: " << sumSquares << std::endl;
+    std::cout << "Factorial of " << n << " is: " << factorial << std::endl;
+
+    return 0;
+   
    
 }
